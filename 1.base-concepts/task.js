2d88a4fe99e1cfc16bc1f8a2b7edfc2a9cc3625b
Yulia Zinoveva <yulchik884@bk.ru>
2022-02-05 01:25:10 +0700

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
+"use strict";
+
 function solveEquation(a, b, c) {
   let arr;
   // код для задачи №1 писать здесь
+  const d = b**2 - (4*a*c);
+
+  if (d === 0) {
+    const x = (-b / (2 * a).toFixed(4));
+    arr = [x];
+  } else if (d > 0) {
+    const x1 = (-b + Math.sqrt(d)) / (2*a).toFixed(4); 
+    const x2 = (-b - Math.sqrt(d)) / (2*a).toFixed(4);
+    arr = [x1, x2];
+  } else {
+    arr = [];
+  }
+  
   return arr; // array
 }
 
