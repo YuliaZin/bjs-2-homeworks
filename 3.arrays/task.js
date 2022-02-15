c4a6ba6717e0c8154d28eed2db054ebefff91cef
Yulia Zinoveva <yulchik884@bk.ru>
2022-02-15 17:32:45 +0700

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,8 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  
+  result = (arr1.every((v, i) => v === arr2[i]) && (arr1.length === arr2.length));
 
   return result; // boolean
 }
@@ -10,6 +12,10 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
+  resultArr = arr
+    .filter((positive) => positive > 0)
+    .filter((dividedThree) => dividedThree % 3 === 0)
+    .map((number) => number * 10);
 
   return resultArr; // array
 }
