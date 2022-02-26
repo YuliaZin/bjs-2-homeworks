7db016723f6de1c50cc151ace3fa4d36725967d5
Yulia Zinoveva <yulchik884@bk.ru>
2022-02-26 15:38:13 +0700

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,47 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
+student1 = new Student("Василий", "муж", "21");
+student2 = new Student("Пётр", "муж", "22");
+student3 = new Student("Ульяна", "жен", "21");
+student4 = new Student("Марина", "жен", "19");
+
+
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
 // ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if ( this.marks === undefined) {
+    this.marks = [mark]; 
+  } else {
+    this.marks.push(mark); 
+  }
+}
+
+Student.prototype.addMarks = function(...mark) {
+  let args = Array.from(mark);
+  this.marks = args; 
+}
+
+Student.prototype.getAverage = function() {
+  let arr = this.marks;
+  let result = 0;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return result = sum / arr.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
