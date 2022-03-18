c2927db541ee9eb622bf72ad3cf1242c0451e1c8
Yulia Zinoveva <yulchik884@bk.ru>
2022-03-18 14:59:49 +0700

    first commit variables

---
+++ 7.async/task.js
@@ -0,0 +1,83 @@
+
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.timerId = null;
+    }
+
+    addClock(time, callback, id) {
+
+        if (!id) {
+        throw new Error('id будильника не передан');
+      }
+
+      if (this.alarmCollection.some(item => item.id === id)) {
+        console.error('Такой id уже есть');
+        return null
+      } 
+      
+      this.alarmCollection.push({
+        time: time, 
+        callback: callback, 
+        id: id
+      });
+      
+    }
+
+
+    removeClock(id){
+    let start = this.alarmCollection.length;
+
+    this.alarmCollection = this.alarmCollection.filter((item) => item.id !== id);       
+    
+
+    let finish = this.alarmCollection.length;
+
+    return (start > finish ) ? true : false;
+    
+    }
+
+    getCurrentFormattedTime() {
+      let date = new Date();
+      let minutes = date.getMinutes();
+      let hours = date.getHours();
+
+      return (hours + ':' + minutes);
+    }
+
+    start() {
+      const checkClock = (callTime) => {
+        if (callTime.clockTime === this.getCurrentFormattedTime()) {
+          callTime.callback();
+        }
+    };
+
+    if (this.timerId === null) {
+        this.timerId = setInterval(() => {
+            this.alarmCollection.forEach((callTime) =>
+                checkClock(callTime)
+            );
+        }, 1000);
+    }
+
+
+  }
+
+    stop() {
+      if (this.timerId !== null) {
+       clearInterval(this.timerId);
+      }
+      this.timerId = null;
+    }
+  
+    printAlarms() {
+      console.log("Печать всех будильников в количестве: " + this.alarmCollection.length);
+      this.alarmCollection.forEach(item => console.log('id звонка: ' + item.id + ', время звонка: ' + item.time));
+    }
+
+    clearAlarms() {
+      this.stop();
+      this.alarmCollection = [];
+    }
+}
+
